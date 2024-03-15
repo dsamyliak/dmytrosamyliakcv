98c066a1648bea6243fdd5fe9c516381949a048a
dsamyliak <dsamyliak@gmail.com>
2024-03-15 17:08:55 +0000

    Deploying to gh-pages from @ dsamyliak/dmytrosamyliak-cv@2239cf9c219dc492f11b900cdba8f76cc30b03fb 🚀

--- src.95ffc9f0.js
+++ src.95ffc9f0.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/dmytrosamyliak-cv/src.95ffc9f0.js.map
+//# sourceMappingURL=/dmytrosamyliak-cv-support/src.95ffc9f0.js.map
